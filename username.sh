142f12dc2adeba5b270b0589cb43f593f6ce5a0c
ruthvekh <ruthvikk1214@gmail.com>
2026-02-08 00:13:19 +0530

    variables

--- username.sh
+++ username.sh
@@ -1,4 +1,7 @@
 !#/bin/bash
 echo "What is your username?"
 read username
-echo "Your username is $username"
+echo "Your username is $username"   
+echo "What is your password?"
+read -s password
+echo "Your password is $password"
