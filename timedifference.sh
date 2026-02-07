2c78f5bc73466938ccc031aa0b63f54a9b75cf3a
ruthvekh <ruthvikk1214@gmail.com>
2026-02-08 00:30:27 +0530

    variables

--- timedifference.sh
+++ timedifference.sh
@@ -3,5 +3,5 @@ Start_time=$(date +%s)
 echo "The Start time in seconds is $Start_time"
 sleep 10
 End_time=$(date +%s)
-diff_time= $((End_time-Start_time))
+diff_time=$((End_time-Start_time))
 echo "The time difference is $diff_time seconds"
