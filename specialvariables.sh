41179c39717149f34fab180824737963c18bc368
ruthvekh <ruthvikk1214@gmail.com>
2026-02-08 00:59:39 +0530

    variables

---
+++ specialvariables.sh
@@ -0,0 +1,12 @@
+!#/bin/bash 
+##special variables##
+echo "All args passed to the script: $@"
+echo "Number of variables passed to the script: $#"
+echo "Script name: $0"
+echo "Present directory: $pwd"
+echo "Who is running: $USER"
+echo "Home directory of the user: $HOME"
+echo "PID of this script: $$"
+sleep 5 &
+echo "PID of recently executed background process: $!"
+echo "All args passed to the script: $*"
