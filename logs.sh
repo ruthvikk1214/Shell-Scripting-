6cf3c17a52daeb622fbf2483aa795169341146e6
ruthvekh <ruthvikk1214@gmail.com>
2026-02-08 14:57:21 +0530

    variables

---
+++ logs.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+USERID=$(id -u)
+
+LOG_FOLDER="/var/log/shell-scrpiting"
+LOG_FILE="/var/log/shell-scrpiting/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run the script with root user access"
+exit 1
+fi
+
+mkdir -m $LOG_FOLDER
+
+Validate(){
+    if [ $1 -ne 0]; then
+    echo "$2 failed"
+    exit 1
+    else
+    echo "$2 success"
+    fi
+    }
+
+dnf install nginx -y
+
+Validate $? "Install nginx" &>> $LOG_FILE
+
+dnf install mysql -y
+
+Validate $? "Install mysql" &>> $LOG_FILE
+
+dnf install nodejs -y
+Validate $? "Install nodejs" &>> $LOG_FILE
