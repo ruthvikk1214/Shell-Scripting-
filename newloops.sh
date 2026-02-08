6e64912f1717993c1d1c7e1d16788bfc22e41f59
ruthvekh <ruthvikk1214@gmail.com>
2026-02-08 18:46:46 +0530

    variables

---
+++ newloops.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+USERID=$(id -u)
+
+LOG_FOLDER="/var/log/shell-scripting"
+LOG_FILE="/var/log/shell-scripting/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run the script with root user access"
+exit 1
+fi
+
+mkdir -p $LOG_FOLDER
+
+Validate(){
+    if [ $1 -ne 0 ] ; then
+    echo "$2 failed"
+    exit 1
+    else
+    echo "$2 success"
+    fi
+    }
+for package in $@
+do 
+dnf install $package -y
+Validate  $? "$package installation"
+done
